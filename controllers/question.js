4fe0bf9ba024d7242ced8992fe4f951d2c3a79f7
buketsoyhan <45885283+buketsoyhan@users.noreply.github.com>
2021-10-12 17:13:45 +0300

    ID'ye göre Name'e göre veri çekildi create ile yeni kişi oluşturuldu.

--- controllers/question.js
+++ controllers/question.js
@@ -12,10 +12,35 @@ const getAllQuestions = (req, res, next) => {
 };
 
 const getData = async (req, res, next) => {
-  const users = await User.find().exec();
-  res.status(200).json({
-    status: "success",
 
+
+
+  const users = await User.find().exec();
+
+  // ** User tablosuna yeni kişi ekleme **
+  //await User.create({name:'Aslı2', password:'vndsvkjsdvdskkls', email:'aslik@gmail.com'})
+
+  // ** İsme göre bulma **
+  // User.find({ name: 'Budket' }).
+  // then(users => {              
+  //   console.log(users[0]); // 'A'
+  //   return users[0];
+  // }).
+  // then(users => {
+  //   console.log(users[0]); // 'B'
+  // });
+
+    // ** İdye göre bulma **
+  // User.find({ id: '6165948ab174345764b2c3a3'}).
+  // then(users => {              
+  //   console.log(users[0]); // 'A'
+  //   return users[0];
+  // }).
+  // then(users => {
+  //   console.log(users[0]); // 'B'
+  // });
+
+  res.status(200).json({
     data: {
       data: users,
     },
